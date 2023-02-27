c1976114ac637861c358c4ec6e88e2fc498e1d43
mutotu <muttalip.aydemir@gmail.com>
2023-02-27 10:41:33 -0800

    attempt mergedded endpoinst for gettins projects

--- group-final/src/main/java/com/cooksys/groupfinal/services/ProjectService.java
+++ group-final/src/main/java/com/cooksys/groupfinal/services/ProjectService.java
@@ -1,5 +1,17 @@
 package com.cooksys.groupfinal.services;
 
+import com.cooksys.groupfinal.dtos.ProjectDto;
+
+import java.util.List;
+import java.util.Set;
+
 public interface ProjectService {
 
+    List<ProjectDto> getAllProjects();
+
+    List<ProjectDto> getAllActiveProjects();
+
+    Set<ProjectDto> getAllTeamProjects(Long id);
+
+//    ProjectDto createProject();
 }
