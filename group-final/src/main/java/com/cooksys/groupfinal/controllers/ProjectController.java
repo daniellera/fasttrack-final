4b6c1d6006f599c3b31dbaee9bc3e574f5d9ac03
mutotu <muttalip.aydemir@gmail.com>
2023-02-27 10:41:33 -0800

    added endpoinst for gettins projects

--- group-final/src/main/java/com/cooksys/groupfinal/controllers/ProjectController.java
+++ group-final/src/main/java/com/cooksys/groupfinal/controllers/ProjectController.java
@@ -1,11 +1,15 @@
 package com.cooksys.groupfinal.controllers;
 
+import com.cooksys.groupfinal.dtos.ProjectDto;
+import com.cooksys.groupfinal.services.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cooksys.groupfinal.services.ProjectService;
-
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/projects")
@@ -13,5 +17,30 @@ import lombok.RequiredArgsConstructor;
 public class ProjectController {
 	
 	private final ProjectService projectService;
+
+	// get all the projects
+
+	@GetMapping
+	public List<ProjectDto> getAllProjects() {
+		return projectService.getAllProjects();
+	}
+
+	//get all the projects that are active
+	@GetMapping("/active")
+	public List<ProjectDto> getAllActiveProjects() {
+		return projectService.getAllActiveProjects();
+	}
+
+	//get all the projects by a team
+	@GetMapping("/team/{id}")
+	public Set<ProjectDto> getAllTeamProjects(@PathVariable Long  id){
+		return projectService.getAllTeamProjects(id);
+	}
+
+	//post a project
+//	@GetMapping("/{id}/post")
+//	public ProjectDto createProject(@PathVariable Long  id, @RequestBody ) {
+//		return projectService.createProject();
+//	}
 
 }
--- group-final/src/main/java/com/cooksys/groupfinal/services/impl/ProjectServiceImpl.java
+++ group-final/src/main/java/com/cooksys/groupfinal/services/impl/ProjectServiceImpl.java
@@ -1,13 +1,54 @@
 package com.cooksys.groupfinal.services.impl;
 
+import com.cooksys.groupfinal.dtos.ProjectDto;
+import com.cooksys.groupfinal.entities.Project;
+import com.cooksys.groupfinal.entities.Team;
+import com.cooksys.groupfinal.exceptions.NotFoundException;
+import com.cooksys.groupfinal.mappers.ProjectMapper;
+import com.cooksys.groupfinal.repositories.ProjectRepository;
+import com.cooksys.groupfinal.repositories.TeamRepository;
+import com.cooksys.groupfinal.services.ProjectService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import com.cooksys.groupfinal.services.ProjectService;
-
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class ProjectServiceImpl implements ProjectService {
 
+    private final ProjectRepository projectRepository;
+    private final ProjectMapper projectMapper;
+    private final TeamRepository teamRepository;
+
+    @Override
+    public List<ProjectDto> getAllProjects() {
+        List <Project> projects = projectRepository.findAll();
+        return projectMapper.entitiesToDtos(projects);
+    }
+
+    @Override
+    public List<ProjectDto> getAllActiveProjects() {
+        List <Project> projects = projectRepository.findAll();
+        List <Project> activeProjects = projectRepository.findAll();
+        for(Project project : projects){
+            if(project.isActive()) activeProjects.add(project);
+        }
+        return projectMapper.entitiesToDtos(activeProjects);
+    }
+
+    @Override
+    public Set<ProjectDto> getAllTeamProjects(Long id) {
+        Optional<Team> team = teamRepository.findById(id);
+        if(team.isEmpty()) throw new NotFoundException("Invalid ID");
+        Set<Project> projects = team.get().getProjects();
+        return projectMapper.entitiesToDtos(projects);
+    }
+
+//    @Override
+//    public ProjectDto createProject() {
+//        return null;
+//    }
 }
