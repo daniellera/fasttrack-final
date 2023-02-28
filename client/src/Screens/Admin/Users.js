cd40c9d8e68fcbf162c031ca0029abe1330cbd64
drouse <drouse90@gmail.com>
2023-02-28 15:25:25 -0500

    Start popup creation

--- client/src/Screens/Admin/Users.js
+++ client/src/Screens/Admin/Users.js
@@ -1,8 +1,12 @@
+import { useState } from 'react'
 import { Navigate } from "react-router-dom"
 import { useRecoilState } from "recoil"
 import NavBar from "../../Components/NavBar"
 import { userState, userRegistryState } from "../../globalstate"
 import styled from 'styled-components'
+import Button from '../../Components/Button'
+import Popup from "../../Components/Popup"
+import Dropdown from '../../Components/Dropdown'
 
 const UserRegistryWrapper = styled.div`
     & a {
@@ -13,7 +17,7 @@ const UserRegistryWrapper = styled.div`
         text-decoration: underline;
     }
 
-    max-width: 80%;
+    max-width: 70%;
     margin: 0 auto;
     padding: 2em;
     font-family: 'Mulish', sans-serif;
@@ -40,9 +44,20 @@ const UserRegistryWrapper = styled.div`
         margin: 0 auto;
         padding-bottom: 2em;
     }
+
+    & button {
+        font-family: 'Mulish';
+        font-size: 12px;
+        padding: 0.5em 1em;
+        border: 1px #323F4B;
+        border-radius: 15px;
+        align-self: flex-start;
+        margin-top: 1em;
+    }
 `
 
 const RegistryTable = styled.table`
+    width: 100%;
     border-radius: 6px;
     border: 1px solid #DEB992;
     padding: 1em 3em;
@@ -50,13 +65,13 @@ const RegistryTable = styled.table`
     font-family: 'Roboto', sans-serif;
     font-size: 18px;
     color: #DEB992;
-    max-width: 80%;
     
     & th {
         color: #FFF;
         font-style: normal;
         font-weight: 500;
         font-size: 1.3em;
+
     }
 
     & th:first-child, td:first-child {
@@ -65,12 +80,12 @@ const RegistryTable = styled.table`
     }
 
     & tr {
-        border: solid #DEB992;
+        border: 1px solid #DEB992;
         border-width: 1px 0 1px 0;
     }
 
-    & td, th {
-        padding: 1em;
+    & td {
+        padding: 1em 0;
     }
 `
 
@@ -84,10 +99,64 @@ const No = styled.td`
     text-transform: uppercase
 `
 
+const AddUserDiv = styled.div`
+
+`
 
 const Users = () => {
     const [user, setUser] = useRecoilState(userState)
     const [userRegistry, setUserRegistry] = useRecoilState(userRegistryState)
+    const [popup, setPopup] = useState({ isToggled: false })
+    const [newUser, setNewUser] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        password: '',
+        isAdmin: false
+    })
+
+    const togglePopup = () => {
+        setPopup(prev => ({
+            ...prev,
+            isToggled: !prev.isToggled
+        }))
+
+        setNewUser({
+            firstName: '',
+            lastName: '',
+            email: '',
+            phone: '',
+            password: '',
+            isAdmin: false
+        })
+    }
+
+    const booleanOptions = (
+        [true, false].map((element, index) => {
+            <option key={index} value={`${element}`}>{`${element}`}</option>
+        })
+    )
+
+    const updateNewUser = event => {
+        console.log(newUser)
+        setNewUser(prev => ({
+            ...prev,
+            [event.target.name]: event.target.value
+        }))
+    }
+
+    const addUser = (
+        <AddUserDiv>
+            <input type='text' name='firstName' placeholder='first name' onChange={updateNewUser} />
+            <input type='text' name='lastName' placeholder='last name' onChange={updateNewUser} />
+            <input type='text' name='email' placeholder='email' onChange={updateNewUser} />
+            <input type='text' name='phone' placeholder='phone' onChange={updateNewUser} />
+            <input type='text' name='password' placeholder='password' onChange={updateNewUser} />
+            <input type='text' name='confirmPassword' placeholder='confirm password' onChange={updateNewUser} />
+            <Dropdown name='isAdmin' id='isAdmin' className='add-user' selectOption={updateNewUser} options={booleanOptions} />
+        </AddUserDiv>
+    )
 
     const mappedUserData = userRegistry.map((element, index) => (
         <tr key={index}>
@@ -121,6 +190,8 @@ const Users = () => {
                             {mappedUserData}
                         </tbody>
                     </RegistryTable>
+                    <Button bg='#1BA098' c='#FFFFFF' w='13em' h='3em' onClick={togglePopup}>ADD USER</Button>
+                    {popup.isToggled && <Popup handleClose={togglePopup} content={addUser} />}
                 </UserRegistryWrapper>
             </div>
         )
