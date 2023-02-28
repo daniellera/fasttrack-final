1e171febe1db17660eb78777dcee5b3145492f03
Jason Layton <16jalayt@gmail.com>
2023-02-27 20:48:39 -0600

    Initial announcement work

--- group-final/src/main/java/com/cooksys/groupfinal/controllers/AnnouncementController.java
+++ group-final/src/main/java/com/cooksys/groupfinal/controllers/AnnouncementController.java
@@ -1,11 +1,10 @@
 package com.cooksys.groupfinal.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.cooksys.groupfinal.dtos.AnnouncementRequestDto;
 import com.cooksys.groupfinal.services.AnnouncementService;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/announcements")
@@ -14,4 +13,9 @@ public class AnnouncementController {
 	
 	private final AnnouncementService announcementService;
 
+	@PostMapping("")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void postAnnouncements(@RequestBody AnnouncementRequestDto announcementRequestDto) {
+		announcementService.postAnnouncement(announcementRequestDto);
+	}
 }
